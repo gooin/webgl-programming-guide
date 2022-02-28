1c3569fc6fc5f378196fe85c0a5176cd7999663c
yanzt <yanzt@gistack.cn>
2022-03-01 07:51:45 +0800

    add glsl loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    // Important: return the modified config
+    config.module.rules.push({
+      test:  /\.(vert|frag|glsl)$/,
+      use: 'raw-loader',
+    })
+    return config
+  }
 }
 
 module.exports = nextConfig
--- pages/Hello.tsx
+++ pages/Hello.tsx
@@ -1,43 +1,43 @@
 import type { NextPage } from 'next';
-import React, { useRef } from 'react';
-import styles from '../styles/Home.module.css';
+import React, { useEffect, useRef } from 'react';
 import { useWebGLInit } from '../hooks';
 import { initShaders } from '../utils/shader_util';
 import Layout from '../components/Layout';
-import { resizeCanvasToDisplaySize } from '../utils/common_util';
 
 // @ts-ignore
-// import VSHADER_SOURCE  from '../shaders/vert/ch2_point.vert';
+import VSHADER_SOURCE  from '../shaders/vert/ch2_point.vert';
 
-function render(gl: WebGLRenderingContext) {
+function useRender(gl: WebGLRenderingContext) {
     console.log('rendergl', gl);
-
-    if (!gl) return;
-    const VSHADER_SOURCE = `
+    useEffect(() => {
+        if (!gl) return;
+        const VSHADER_SOURCE1 = `
     void main() {
         gl_Position = vec4(0.0,0.0,0.0,1.0);
         gl_PointSize = 10.0;
     }
     `;
-    const FSHADER_SOURCE = `
+        const FSHADER_SOURCE = `
         void main() {
             gl_FragColor = vec4(1.0,1.0,0.0,1.0);
         }
         `;
 
-    initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
+        initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
 
-    gl.clearColor(0.0, 0.0, 0.0, 0.0);
-    // 清空canvas
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    //绘制点
-    gl.drawArrays(gl.POINTS, 0, 1);
+        gl.clearColor(0.0, 0.0, 0.0, 0.0);
+        // 清空canvas
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        //绘制点
+        gl.drawArrays(gl.POINTS, 0, 1);
+    }, [gl]);
+
 }
 
 const Hello: NextPage = () => {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
     const gl = useWebGLInit(canvasRef) as WebGLRenderingContext;
-    render(gl);
+    useRender(gl);
     return (
         <Layout title={'Hello'}>
             <canvas id={'webgl'} ref={canvasRef}/>
