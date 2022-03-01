08e19fd41c3288968ab7583a266f9176f12abec3
yanzt <yanzt@gistack.cn>
2022-03-01 08:17:42 +0800

    ch4： 平移然后旋转Matrix4

---
+++ pages/ch4/TranslateRotate.tsx
@@ -0,0 +1,61 @@
+import type { NextPage } from 'next';
+import React, { useEffect, useRef } from 'react';
+import { initShaders, initVertexBuffers } from '../../utils/shader_util';
+import { useWebGLInit } from '../../hooks';
+import Layout from '../../components/Layout';
+import { Matrix4 } from '../../utils/matrix4_util';
+
+function useRender(gl: WebGLRenderingContext) {
+    console.log('rendergl', gl);
+    useEffect(() => {
+        if (!gl) return;
+        const VSHADER_SOURCE = `
+            attribute vec4 a_Position;
+            uniform mat4 u_xformMatrix;
+            void main() {
+                gl_Position = u_xformMatrix * a_Position;
+            }
+    `;
+        const FSHADER_SOURCE = `
+        void main() {
+            gl_FragColor = vec4(1.0,1.0,0.0,1.0);
+        }
+        `;
+
+        initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
+        const n = initVertexBuffers(gl);
+        if (n < 0) {
+            console.log('Failed to set the positions of the vertices');
+            return;
+        }
+
+        // 新建一个矩阵
+        const xformMatrix = new Matrix4();
+        // 设置旋转
+        xformMatrix.setRotate(90.0, 0.0, 0.0, 1.0);
+
+        const u_xformMatrix = gl.getUniformLocation(gl.program, 'u_xformMatrix');
+
+        gl.uniformMatrix4fv(u_xformMatrix, false, xformMatrix.elements);
+
+        gl.clearColor(0.0, 0.0, 0.0, 0.0);
+        // 清空canvas
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        //绘制点
+        gl.drawArrays(gl.TRIANGLES, 0, n);
+    }, [gl]);
+
+}
+
+const TranslateRotate: NextPage = () => {
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const gl = useWebGLInit(canvasRef) as WebGLRenderingContext;
+    useRender(gl);
+    return (
+        <Layout title={'TranslateRotate'}>
+            <canvas id={'webgl'} ref={canvasRef}/>
+        </Layout>
+
+    );
+};
+export default TranslateRotate;
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,47 +14,28 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <Link href="/Hello">
-          <a>Hello</a>
-        </Link>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          WebGL 编程指南笔记
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className={styles.card}>
+            <h2>HelloWebGL &rarr;</h2>
+            <Link href="/Hello">
+              <a>TranslateRotate</a>
+            </Link>
+          </div>
+          <div className={styles.card}>
+            <h2>高级变换与动画基础 &rarr;</h2>
+            <Link href="/ch4/TranslateRotate">
+              <a>平移，然后旋转</a>
+            </Link>
+          </div>
         </div>
+
       </main>
 
       <footer className={styles.footer}>
